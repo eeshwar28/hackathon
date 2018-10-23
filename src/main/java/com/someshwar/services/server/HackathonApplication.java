d657628cb5a21466c2fbaf888ba11556e53aaffe
eeshwar28 <eeshwar28@gmail.com>
2018-10-23 13:08:42 -0700

    removed class

--- src/main/java/com/someshwar/services/server/HackathonApplication.java
+++ src/main/java/com/someshwar/services/server/HackathonApplication.java
@@ -8,19 +8,18 @@ import javax.ws.rs.core.Application;
 
 import org.springframework.stereotype.Component;
 
-import com.americanexpress.hackthon.restapi.controller.HelloWorldController;
+import com.someshwar.services.controller.HelloController;
 
 @Component
 @ApplicationPath("/")
 public class HackathonApplication extends Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
-       
-        Set<Class<?>> classesSet = new HashSet<Class<?>>();
-        classesSet.add(HelloWorldController.class);
-        
-        
-        return classesSet;
-    }
+	@Override
+	public Set<Class<?>> getClasses() {
+
+		Set<Class<?>> classesSet = new HashSet<Class<?>>();
+		classesSet.add(HelloController.class);
+
+		return classesSet;
+	}
 }
